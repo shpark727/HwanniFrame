f4d90d694d0e265d7d5060f1405a6139f6e1831c
박성환 <p@bagseonghwan-ui-MacBook-Pro.local>
2017-07-05 20:55:21 +0900

    Add .travis.yml

---
+++ build/HwanniFrame.build/Release-iphoneos/HwanniFrame.build/DerivedSources/HwanniFrame_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char HwanniFrameVersionString[];
+ extern const double HwanniFrameVersionNumber;
+
+ const unsigned char HwanniFrameVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:HwanniFrame  PROJECT:HwanniFrame-1" "\n";
+ const double HwanniFrameVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/HwanniFrame.build/Release-iphoneos/HwanniFrame.build/Objects-normal/arm64/HwanniFrame_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/p/Desktop/HwanniFrame/build/HwanniFrame.build/Release-iphoneos/HwanniFrame.build/DerivedSources/HwanniFrame_vers.c
